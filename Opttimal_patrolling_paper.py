6d29e296917dfe5a787edcb32bca86b4367905a3
Rugved Katole <f20180391@goa.bits-pilani.ac.in>
2022-01-07 21:39:42 +0530

    removed hardcode

--- Opttimal_patrolling_paper.py
+++ Opttimal_patrolling_paper.py
@@ -1,16 +1,20 @@
+#!/usr/bin/env python3
+
+
 import networkx as nx
 import numpy as np
-from collections import defaultdict
-from matplotlib import pyplot as plt
+from mrpp_sumo.srv import NextTaskBot, NextTaskBotResponse, AlgoReady, AlgoReadyResponse
+from mrpp_sumo.msg import AtNode
 import sys
+import rospy
+import rospkg
 
-
-#Graph_path= input("please provide the path to GraphML file ")
+"""we need DFS tour not justs search"""
+''' add call back next task'''
+'''
 Graph_path = sys.argv[1]
-#no_off_agents= int(input("Please provide number of agents no "))
 no_off_agents = int(sys.argv[2])
-GraphML= nx.read_graphml(Graph_path)          #Reading the GraphML file from the given input path
-
+graphML= nx.read_graphml(Graph_path) '''         #Reading the graphML file from the given input path
 def DFSUtil(Graph, v, visited):
     '''The helper Recursive Function for DFS traversel'''
         # Mark the current node as visited and print it
@@ -21,6 +25,7 @@ def DFSUtil(Graph, v, visited):
     for neighbour in Graph.neighbors(v):
         if neighbour not in visited:
             DFSUtil(Graph,neighbour, visited)
+        
     return visited
 
 
@@ -36,138 +41,106 @@ def DFS (Graph):
             DFSUtil(Graph,vertex, visited)
     return visited
 
-def partition(G,l):
-  Weight = "lenght"
+def partition(g,wg,l):
   a=0
   pi=[]
-  #print("l= ",l)
-  while a<wg[-1]:
-    #p = [(u,v) for (u,v,d) in G.edges(data=True) if d[Weight] <=(a+l) and d[Weight] >=a]
-    p = [(u) for (u,d) in G.nodes(data=True) if d[Weight] <=(a+l) and d[Weight] >=a]
+  while a<=wg[-1]:
+    p = [(u) for (u) in g if wg[g.index(u)] <=(a+l) and wg[g.index(u)] >=a]
     pi.append(p)
-    #print(pi)
-    #a =[(u,v) for (u,v,d) in G.edges(data=True) if d[Weight]>=(a+l)]
-    a = [(u) for (u,d) in G.nodes(data = True) if d[Weight] >= (a+l)]
+    a = [(u) for (u) in g if wg[g.index(u)] >= (a+l)]
     c = []
     for u in a:
-      c.append(G.nodes[u][Weight])
-    #print("c=",c)
+      c.append(wg[g.index(u)])
     if len(c)==0:
       return pi
     else:
       a=min(c)
-      #print("a=",a)
   return pi
 
-#Graph=nx.Graph()
-#elist = [(1,2, 1), (2,3, 2), (3,4, 4), (4,5, 5),(5,6,7),(6,7, 8),(7,8, 10),(8,9, 13),(9,10, 14)]
-#Graph.add_weighted_edges_from(elist)
-G=DFS(GraphML) 
-wg=[]
-for i in range(len(G)):
-  cur=G[0]
-  #dist=nx.dijkstra_path_length(GraphML, cur, G[i], weight = 'length')   #Dijkstra path lenght funtion
-  dist = (i)*100  #lenght of each edge is 100 which is added in chain graph.
-  wg.append(dist)
-#G=range(len(G))
-G=list(map(int,G))
-wg=list(map(int,wg))
-elist=[]
-for i in range(len(G)-1):
-  #elist.append((G[i],G[i+1],wg[i+1]))
-  elist.append((G[i],G[i+1]))
-#print("edgelist= ",elist)
-Graph=nx.Graph()
-#Graph.add_weighted_edges_from(elist)
-Graph.add_edges_from(elist)
-j=0
-for i in Graph.nodes():
-  Graph.nodes[i]["lenght"]=100*j
-  j+=1
-#print(Graph.edges(data=True))
-#print(Graph.nodes(data=True))
+
 
 '''setting the parameters for partitioning'''
-a = 0
-delta = 100
-epsilon = 0.01
-m = no_off_agents
-weight = "lenght"
-b = (wg[-1]+delta)/m
-l = (a+b)/2
-while (b-a)>2*epsilon:
-  pi = partition(Graph,l)
-  if pi==None:
-    print("No partiontions available at current setting. Please use different Parameters")
-    break
-  if len(pi)>m:
-    a=l
-    l = (a + b)/2
-#    print(pi)
-  else:
-#    print(pi)
-    pis=[]
-    #for i in pi:
-      #pis.append(Graph.edge_subgraph(i))
-      #pis.append(Graph.subgraph(i))
-    pis.append(pi)
-    b = l
-    l = (a+b)/2
-
-for i in pis[0]:
-  #print(i.edges())
-  print(i)
-
-#idelness
-#MAX idealness
-max_idle = -np.inf
-for i in pis[0]:
-  idle = (len(i)-1)*20
-  if idle > max_idle:
-    max_idle = idle
-
-print("Max Idelness =",max_idle, "seconds")
-
-#average Idealness 
-'''adding idleness key to graphs'''
-for i in Graph.nodes():
-  Graph.nodes[i]["idleness"]=0
-
-def update_idleness(graph,node_list,cur_node):
-  sum_idle=0
-  for i in node_list:
-    if i==cur_node:
-      graph.nodes[i]["idleness"] = 0
+def optimal_partition(graphML,m):
+  g=DFS(graphML) 
+  wg=[0]
+  for i in range(1,len(g)):
+    dist = (i)*graphML.edges[g[i-1],g[i]]["length"]  #lenght of each edge is 100 which is added in chain graph. 
+    wg.append(dist)
+  #g=range(len(g))
+  g=list(map(int,g))
+  wg=list(map(int,wg))
+  a = 0
+  delta = 100
+  epsilon = 0.01
+  b = (wg[-1]+delta)/m
+  l = (a+b)/2
+  while (b-a)>2*epsilon:
+    pi = partition(g,wg,l)
+    if len(pi)>m:
+      a=l
+      l = (a + b)/2
+  #    print(pi)
     else:
-      graph.nodes[i]["idleness"] += 10
-      sum_idle += graph.nodes[i]["idleness"]
-  return sum_idle
-  
+  #    print(pi)
+      pis=pi
+      b = l
+      l = (a+b)/2
+  return pis
 
-average_idle=0
+'''
+def callback_idle(self, data):
+  if self.stamp < data.stamp:
+    dev = data.stamp - self.stamp
+    self.stamp = data.stamp
+    for n in range(num_bots):
+      for i in self.nodes:
+        self.robots['bot_{}'.format(n)][i] += dev
+                
+    for i, n in enumerate(data.robot_id):
+      self.robots[n][data.node_id[i]] = 0.'''
 
-for i in pis[0]:
-  I = 0
-  for j in i[1:]:
-    sum_idle1 = update_idleness(Graph,i,j) #sum of instaneous idleness of a partition at every timestep of 10s I=sumation(I(t))
-    I += sum_idle1    
-  i.reverse()
-  for j in i[1:]:
-    sum_idle2=update_idleness(Graph,i,j)  #same thing for returning to start node
-    I += sum_idle2
-  try:
-    average_idle += I/(len(i)*(len(i)-1)*20)  # average graph idleness I=sumation(I(t))/no_of_nodes/total_time in each partition
-  except (ZeroDivisionError):
-    continue
+class OPP:
 
-
-print("Average graph idleness =",average_idle,"seconds")
-  
-
-
-
+  def __init__(self, g, num_bots):
+    self.graph=g
+    self.num_bots = num_bots
+    self.stamp = 0
+    rospy.Service('algo_ready', AlgoReady, self.callback_ready)
+    self.ready = True
 
 
-#print(Graph.edges(data=True))
-#nx.draw(GraphML)
-#plt.show()
+  def callback_ready(self, req):
+    algo_name = req.algo
+    if algo_name == 'Opttimal_patrolling_paper' and self.ready:
+      return AlgoReadyResponse(True)
+    else:
+      return AlgoReadyResponse(False)
+
+  def callback_next_task(self,req):
+    node = req.node_done
+    t = req.stamp
+    bot = req.name
+    pis=optimal_partition(self.graph,self.num_bots)
+    node_list = pis[int(bot[-1])]
+    if int(node)+5<=24:
+      neigh = str(int(node)+5)
+    else:
+      neigh = str(int(node)-5)
+    next_walk = [node, neigh]
+    next_departs = [t]
+    return NextTaskBotResponse(next_departs, next_walk)
+
+if __name__ == '__main__':
+  rospy.init_node('optimal', anonymous= True)
+  dirname = rospkg.RosPack().get_path('mrpp_sumo')
+  done = False
+  graph_name = rospy.get_param('/graph')
+  num_bots = int(rospy.get_param('/init_bots'))
+  g = nx.read_graphml(dirname + '/graph_ml/' + graph_name + '.graphml')
+  s=OPP(g,num_bots)
+  #rospy.Subscriber('at_node', AtNode, s.callback_idle)
+  rospy.Service('bot_next_task', NextTaskBot, s.callback_next_task)
+
+  done = False
+  while not done:
+    done = rospy.get_param('/done')
